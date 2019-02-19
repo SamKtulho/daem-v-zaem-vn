7e5696116ffd8efd5fe1941b7fb27c08f068ed1a
German <sam@coccoc.com>
2019-02-19 14:07:44 +0700

    fix links

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
     ];
     
     $links = [
-        'doctor' => 'https://fast.accesstrade.com.vn/deep_link/5060554555587958166?url=https%3A%2F%2Fcamdo.vietmoney.vn%2F&utm_campaign=' . config('app.site_name') . '&utm_source=' . config('app.sub2', 'sub2'),
+        'doctor' => $doctorDongLinks[array_rand($doctorDongLinks)],
         'vietmoney' => 'https://fast.accesstrade.com.vn/deep_link/5060554555587958166?url=https%3A%2F%2Fcamdo.vietmoney.vn%2F&utm_campaign=' . config('app.site_name') . '&utm_source=' . config('app.sub2', 'sub2'),
         'avay' => 'https://fast.accesstrade.com.vn/deep_link/5060554555587958166?url=https%3A%2F%2Favay.vn%2F&utm_campaign=' . config('app.site_name') . '&utm_source=' . config('app.sub2', 'sub2'),
         'comb' => 'https://fast.accesstrade.com.vn/deep_link/5060554555587958166?url=https%3A%2F%2Fsaleonline.com-b.vn%2F&utm_campaign=' . config('app.site_name') . '&utm_source=' . config('app.sub2', 'sub2'),
@@ -35,14 +35,14 @@ Route::get('/', function () {
         'f88' => 'https://fast.accesstrade.com.vn/deep_link/5060554555587958166?url=http%3A%2F%2Fvaynongoto.f88.vn%2F&utm_campaign=' . config('app.site_name') . '&utm_source=' . config('app.sub2', 'sub2'),
     ];
 
-    $links = [
+/*    $links = [
         'doctor' => 'https://camdo.vietmoney.vn/?aff_sid=jNcXRPTXrnTG9s8xVdL1xbZZey86vggCCss0pq48RZAyHLw8',
         'vietmoney' => 'https://camdo.vietmoney.vn/?aff_sid=jNcXRPTXrnTG9s8xVdL1xbZZey86vggCCss0pq48RZAyHLw8',
         'avay' => 'https://avay.vn/?utm_source=accesstrade&aff_sid=6flehhsWeyeIyt6UbhSXNxzUGyAnTpzLUHpcZGedswUiTZnr',
         'comb' => 'https://saleonline.com-b.vn/?utm_source=accesstrade&aff_sid=poLjHuWKIBBya4c1jSDD8h5Ch3Ohy1AJadTGUD7WcKeNYsOH',
         'shb' => 'http://www.vaytienmat.shbfinance.com.vn/?utm_campaign=ACT&utm_medium=Allifiliate&utm_source=ACT&aff_sid=vrBPGV7219Noj1U7QjfHny0XdhQOnJWLbavzdJK0gKR9pBzw',
         'f88' => 'https://vaynongoto.f88.vn/?utm_source=accesstrade&aff_sid=GKPFNla0M8dU8oWC3NySOsKhyOMm3rtpML6uZauEqDFZRR1J',
-    ];
+    ];*/
     
     return view('main', ['city' => $city, 'links' => $links]);
 });
